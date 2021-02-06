13a6fbb0320176d5300ec153e7fd1f53161fe528
pleasecallmekaige <2964073182@qq.com>
2021-02-06 18:35:04 +0800

    build success

---
+++ envsetup.sh
@@ -0,0 +1,11 @@
+export BOOSTDIR=/d/Lib/boost_1_75_0
+
+export QT_BIN_PATH=/e/software/normal/Qt5/5.12.9/mingw73_32/bin
+export QT_MINGW32_BIN_PATH=/E/software/normal/Qt5/Tools/mingw730_32/bin
+
+export PATH=$QT_MINGW32_BIN_PATH:$QT_BIN_PATH:$PATH
+
+export CROSS_COMPILE=i686-w64-mingw32-
+
+export QMAKE=$QT_BIN_PATH/qmake
+export RELEASE_PATH=./release
--- release-win32.sh
+++ release-win32.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+export BOOSTDIR=/d/Lib/boost_1_75_0
+export QT_BIN_PATH=/e/software/normal/Qt5/5.12.9/mingw73_32/bin
+export QT_MINGW32_BIN_PATH=/E/software/normal/Qt5/Tools/mingw730_32/bin
+export PATH=$QT_MINGW32_BIN_PATH:$QT_BIN_PATH:$PATH
+export CROSS_COMPILE=i686-w64-mingw32-
+export QMAKE=$QT_BIN_PATH/qmake
+export RELEASE_PATH=./release
 
-BOOSTDIR=$HOME/boost_1_43_0
+BOOSTDIR=/d/Lib/boost_1_75_0
 
-PATH=/cygdrive/c/qt/2010.02.1/qt/bin:/cygdrive/c/qt/2010.02.1/mingw/bin/:$PATH
 if [ -z "$VERSION" ]; then
     echo "Building default version"
     qmake glogg.pro -spec win32-g++ -r CONFIG+=release BOOST_PATH=$BOOSTDIR
@@ -11,10 +17,11 @@ else
     qmake glogg.pro -spec win32-g++ -r CONFIG+=release BOOST_PATH=$BOOSTDIR VERSION="$VERSION"
 fi
 mingw32-make
-cp /cygdrive/c/qt/2010.02.1/qt/bin/{QtCore4,QtGui4}.dll release/
-cp /cygdrive/c/qt/2010.02.1/mingw/bin/{mingwm10,libgcc_s_dw2-1}.dll release/
+
+cp $QT_MINGW32_BIN_PATH/{Qt5Core,Qt5Gui,Qt5Widgets,Qt5Network,libgcc_s_dw2-1,libwinpthread-1,libstdc++-6}.dll $RELEASE_PATH/
+
 if [ -z "$VERSION" ]; then
     VERSION=`git describe`;
 fi
-echo Generating installer for glogg-$VERSION
-/cygdrive/c/Program\ Files/NSIS/makensis -DVERSION=$VERSION glogg.nsi
+# echo Generating installer for glogg-$VERSION
+# /cygdrive/c/Program\ Files/NSIS/makensis -DVERSION=$VERSION glogg.nsi
